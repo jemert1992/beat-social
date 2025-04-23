a35bed6527b5a5245fd6a9894919de7edbf0a51c
jemert1992 <justin@tapp.co>
2025-04-23 12:32:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+# Add the parent directory to the Python path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import threading
 import time
 import logging
